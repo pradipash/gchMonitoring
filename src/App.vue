836afa1b426c76ee7d37b3a71dd4fc81e406fe5a
Shukonee <pradipa.setia@gmail.com>
2025-07-08 11:20:38 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const isDone = ref(false)
+</script>
+
+<template>
+  <header>
+    <h1>Muh Games Tracker</h1>
+  </header>
+
+  <div>
+    <label>
+      <p>{{ isDone ? 'Done!' : 'Not Yet!' }}</p>
+    </label>
+  </div>
+</template>
